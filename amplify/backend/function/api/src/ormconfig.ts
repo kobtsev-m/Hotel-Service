0a9e6cd3fb65c772bdbd47e7bb577aa69d44117b
kobtsev-m <kobtsev.info@gmail.com>
2022-03-28 14:58:15 +0700

    Adding .env.example / Update of backend deployment & Typeorm configs

--- amplify/backend/function/api/src/ormconfig.ts
+++ amplify/backend/function/api/src/ormconfig.ts
@@ -1,13 +1,15 @@
 import 'dotenv/config';
 import path from 'path';
 
+const fileExt = process.env.ENV === 'local' ? '.ts' : '.js';
+
 export = {
   type: process.env.DB_TYPE,
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  entities: [path.join(__dirname, 'app', 'db', 'entities', '!(index){.js,.ts}')],
-  migrations: [path.join(__dirname, 'app', 'db', 'migrations', '*{.js,.ts}')],
+  entities: [path.join(__dirname, 'app', 'db', 'entities', `!(index)${fileExt}`)],
+  migrations: [path.join(__dirname, 'app', 'db', 'migrations', `*${fileExt}`)],
   synchronize: false
 };
